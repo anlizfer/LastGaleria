c8a9faa09933bc941c0b3837f7be3bc6de6ce54c
anlizfer <anlizfer@hotmail.com>
2018-07-15 11:27:05 -0500

    sdsd
    sdsd

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -33,6 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
+        get_noticias();
         app.receivedEvent('deviceready');
 
         var notificationOpenedCallback = function(jsonData) {
@@ -68,3 +70,58 @@ document.addEventListener('deviceready', function () {
   
   
 }, false);
+
+var puerto_servidor="";
+var servidor_ws="http://galeria.aedigital.co"+puerto_servidor;
+var zoom_mapa=13;
+var transicion="slide";
+
+function get_noticias(){
+   var request = $.ajax({
+    url: servidor_ws+"/get_notificaciones.php",
+    type: "POST",
+    data: {            
+            idusuario:"1"
+          }
+    });
+
+
+  request.done(function(msg) {            
+    //var obj = jQuery.parseJSON(msg);       
+      if(msg.status=="ok"){
+          
+          var objdata=msg.datos;
+
+          var cadena_noticias="";
+
+
+          for(var i=0; i<objdata.length; i++){
+
+              cadena_noticias+='<div class="row" style="border-bottom: 1px solid #ccc; padding-bottom: 5px;" onclick="get_noticias_detalle('+objdata[i].idnoticias+')">';
+              cadena_noticias+='  <div class="col-xs-3" style="text-align: center;" >';
+              cadena_noticias+='    <div style="    margin-top: 29px; margin-left: 11px; margin-top:10px; border-radius: 60px; background-color: #f8ea26;  width: 60px; height: 60px; background-position: center; background-size: 45px 45px; background-image: url(assets/img/boton_campana.png);">';
+              cadena_noticias+='    </div>';
+              cadena_noticias+='  </div>';
+              cadena_noticias+='  <div class="col-xs-9" >';
+              cadena_noticias+='    <h4>'+objdata[i].tituloNoticia+'</h4>';
+              cadena_noticias+='    <p style="font-size: 12px;">'+objdata[i].descripcionNoticia+'</p>';
+              cadena_noticias+='    <i>Fecha: '+objdata[i].fechaNoticia+'</i>';
+              cadena_noticias+='  </div>';
+              cadena_noticias+='</div>';
+              cadena_noticias+='';
+
+           
+          } 
+
+          $("#listado-noticias").html(cadena_noticias);
+
+      }else{
+          abrir_mensajes("Error",msg.mensaje);
+      }
+  });     
+
+    //respuesta si falla
+    request.fail(function(jqXHR, textStatus) {
+       abrir_mensajes("Error","No se ha podido conectar con el servidor, revise su conexión a internet y pruebe nuevamente.");
+    });
+}
